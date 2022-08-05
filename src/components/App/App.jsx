62f4f247bfe974eb647e8b3864223fa28535a16f
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-08-05 23:17:37 +0300

    Moved api in separate file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import ImageGallery from "../ImageGallery";
 import Loader from '../Loader';
 import Button from '../Button';
 import Modal from '../Modal';
-
+import fetchImages from '../../services';
 
 export class App extends Component {
   state = {
@@ -18,25 +18,21 @@ export class App extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    const API_KEY = '22781965-6f9dce81fb9780324cf438200';
     const { inputValue, page } = this.state;
-
         if (prevState.inputValue !== inputValue || prevState.page !== page) {
 
-            this.setState({ loading: true });
+          this.setState({ loading: true });
 
-          fetch(`https://pixabay.com/api/?q=${this.state.inputValue}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
-            .then(r => r.json())
-            .then(images =>
-            {
+          fetchImages(inputValue, page)
+            .then(images => {
               if (prevState.inputValue === inputValue) {
-              this.setState({ images: [...prevState.images, ...images.hits] });
+                this.setState({ images: [...prevState.images, ...images.hits] });
               } else {
                 this.setState({ images: images.hits });
               }
             })
             .finally(() => this.setState({ loading: false }));
-        } 
+    };
   };
   
   submitHandler = (value) => {
